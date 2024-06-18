be4c9c4ef37832c27e97e5232f64e5dae57641bf
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2024-06-18 18:42:51 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ Config.Debug = false
 Config.Standalone = false
 
 -- Enable outlines around the entity you're looking at
-Config.EnableOutline = false
+Config.EnableOutline = true
 
 -- Whether to have the target as a toggle or not
 Config.Toggle = false
@@ -39,13 +39,13 @@ Config.DrawSprite = true
 Config.DrawDistance = 10.0
 
 -- The color of the sprite in rgb, the first value is red, the second value is green, the third value is blue and the last value is alpha (opacity). Here is a link to a color picker to get these values: https://htmlcolorcodes.com/color-picker/
-Config.DrawColor = { 255, 255, 255, 255 }
+Config.OutlineColor = { 55, 94, 151, 1 }
 
 -- The color of the sprite in rgb when the PolyZone is targeted, the first value is red, the second value is green, the third value is blue and the last value is alpha (opacity). Here is a link to a color picker to get these values: https://htmlcolorcodes.com/color-picker/
-Config.SuccessDrawColor = { 220, 20, 60, 255 }
+Config.SuccessDrawColor = { 255, 255, 255, 1 }
 
 -- The color of the outline in rgb, the first value is red, the second value is green, the third value is blue and the last value is alpha (opacity). Here is a link to a color picker to get these values: https://htmlcolorcodes.com/color-picker/
-Config.OutlineColor = { 255, 255, 255, 255 }
+Config.OutlineColor = { 55, 94, 151, 1 }
 
 -- Enable default options (Toggling vehicle doors)
 Config.EnableDefaultOptions = true
